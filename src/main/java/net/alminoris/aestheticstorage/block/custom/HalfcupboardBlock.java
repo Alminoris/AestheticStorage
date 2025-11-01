249696c7410db382b6405fcc97a67dd60edbffc4
AOA <aoa@d.co>
2025-11-01 19:32:20 +0200

    #2.0.0 Update

--- src/main/java/net/alminoris/aestheticstorage/block/custom/HalfcupboardBlock.java
+++ src/main/java/net/alminoris/aestheticstorage/block/custom/HalfcupboardBlock.java
@@ -43,7 +43,7 @@ public class HalfcupboardBlock extends BlockWithEntity implements BlockEntityPro
 {
     public static final int MAX_STACK_HEIGHT = 4;
 
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0D, 0D, 9D, 16D, 16D, 16D);
+    private static final VoxelShape SHAPE = Block.createCuboidShape(0D, 0D, 0D, 16D, 16D, 7D);
 
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
