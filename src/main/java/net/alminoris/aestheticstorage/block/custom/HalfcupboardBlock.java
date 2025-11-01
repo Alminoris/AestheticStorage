020210808e7033ac16ec872cebfda4f67db9c389
AOA <aoa@d.co>
2025-11-01 21:54:58 +0200

    #2.0.0 Update

--- src/main/java/net/alminoris/aestheticstorage/block/custom/HalfcupboardBlock.java
+++ src/main/java/net/alminoris/aestheticstorage/block/custom/HalfcupboardBlock.java
@@ -2,6 +2,7 @@ package net.alminoris.aestheticstorage.block.custom;
 
 import net.alminoris.aestheticstorage.block.entity.HalfcupboardBlockEntity;
 import net.alminoris.aestheticstorage.block.entity.ModBlockEntities;
+import net.alminoris.aestheticstorage.util.helper.VoxelShapeHelper;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
@@ -20,6 +21,7 @@ import net.minecraft.tag.TagKey;
 import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.shape.VoxelShape;
@@ -79,22 +81,32 @@ public class HalfcupboardBlock extends BlockWithEntity implements BlockEntityPro
         builder.add(FACING, WATERLOGGED, VARIANT, OPEN, FLIPPED, MANUAL_FLIPPED);
     }
 
+    private VoxelShape getRotatedShape(BlockState state)
+    {
+        Direction direction = state.get(FACING);
+
+        List<Box> boxes = new ArrayList<>();
+        boxes.add(SHAPE.getBoundingBox());
+
+        return VoxelShapeHelper.rotateShape(boxes, direction);
+    }
+
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
     {
-        return SHAPE;
+        return getRotatedShape(state);
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
     {
-        return SHAPE;
+        return getRotatedShape(state);
     }
 
     @Override
     public VoxelShape getCameraCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
     {
-        return SHAPE;
+        return getRotatedShape(state);
     }
 
     @Override
